a76af8c149c3a4d8f082634d8959702e81fc7086
Eduardo Domingos <epcdomingos@gmail.com>
2016-08-05 00:01:00 +0100

    Enqueue js again

--- themes/RMConsulting/inc/enqueue-scripts.php
+++ themes/RMConsulting/inc/enqueue-scripts.php
@@ -7,7 +7,7 @@ if ( ! function_exists( 'rm_scripts' ) ) :
 	function rm_scripts() {
 		wp_enqueue_style( 'rm-style', get_template_directory_uri() . '/assets/build/css/main.css' );
 
-		//wp_enqueue_script( 'rm-js', get_template_directory_uri() . '/assets/build/js/main.js', array( 'jquery' ), '', true );
+		wp_enqueue_script( 'rm-js', get_template_directory_uri() . '/assets/build/js/main.js', array( 'jquery' ), '', true );
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'rm_scripts' );
