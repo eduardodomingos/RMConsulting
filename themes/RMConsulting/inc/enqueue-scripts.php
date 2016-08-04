057757b3a3059025de0e658626765daf0cbd9196
Eduardo Domingos <edomingos@impresa.pt>
2016-08-04 17:37:31 +0100

    Reorganize functions.php

---
+++ themes/RMConsulting/inc/enqueue-scripts.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Enqueue scripts and styles.
+ */
+if ( ! function_exists( 'rm_scripts' ) ) :
+	function rm_scripts() {
+		wp_enqueue_style( 'rm-style', get_template_directory_uri() . '/assets/build/css/main.css' );
+
+		wp_enqueue_script( 'rm-js', get_template_directory_uri() . '/assets/build/js/main.js', array( 'jquery' ), '', true );
+	}
+endif;
+add_action( 'wp_enqueue_scripts', 'rm_scripts' );
