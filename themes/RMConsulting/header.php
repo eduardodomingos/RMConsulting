7d2ff1cf432a14c563d20b2c4793d4a6717753f3
Eduardo Domingos <edomingos@impresa.pt>
2016-08-09 14:58:38 +0100

    WIP: single pages

--- themes/RMConsulting/header.php
+++ themes/RMConsulting/header.php
@@ -19,7 +19,6 @@
 
 <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 
 <header class="page-head" role="banner">
--- themes/RMConsulting/inc/hooks.php
+++ themes/RMConsulting/inc/hooks.php
@@ -17,16 +17,13 @@ add_filter( 'excerpt_length', 'rm_custom_excerpt_length', 999 );
 
 
 /**
- * Filter the "read more" excerpt string link to the post.
+ * Filter the excerpt "read more" string.
  *
  * @param string $more "Read more" excerpt string.
  * @return string (Maybe) modified "read more" excerpt string.
  */
-//function rm_excerpt_more( $more ) {
-//	return sprintf( '... <a class="read-more" href="%1$s">%2$s</a>',
-//		get_permalink( get_the_ID() ),
-//		__( 'Ver mais', 'rm' )
-//	);
-//}
-//add_filter( 'excerpt_more', 'rm_excerpt_more' );
+function rm_excerpt_more( $more ) {
+	return '...';
+}
+add_filter( 'excerpt_more', 'rm_excerpt_more' );
 
